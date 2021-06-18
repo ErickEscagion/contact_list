9a74205a710298fa7e7445ab8f610281c2f24a3e
Erick Escagion <erickeescagion@gmail.com>
2021-06-18 13:00:05 -0300

    alter name component "Button" to "Main"

---
+++ src/components/Main.js
@@ -0,0 +1,40 @@
+import React, {useState} from 'react'
+import{Redirect} from 'react-router-dom'
+import Table from './Table';
+
+const Main = () =>{
+
+    const [redirectToRegister, setRedirectToRegister] = useState(false);
+    const [redirectToChange, setRedirectToChange] = useState(false);
+
+    const register = () =>{
+        setRedirectToRegister(true);
+    }
+
+    const change = () =>{
+        setRedirectToChange(true);
+    }
+
+    const renderRedirect = () =>{
+        if(redirectToRegister === true){
+            return <Redirect to='/register'/>;
+        }else if(redirectToChange === true){
+            return <Redirect to='/change'/>;
+        }
+        return null;
+    }
+
+    return (
+        <>
+        {renderRedirect()}
+        <div className="main">
+            <input type="button" value="Cadastrar" onClick={register}></input>
+            <input type="button" value="Alterar" onClick={change}></input>
+            <input type="button" value="Excluir"></input>
+        </div> 
+        <Table></Table>
+        </>
+    )
+} 
+
+export default Main;
