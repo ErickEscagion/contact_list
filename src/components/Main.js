4f636133ceac87e25171095bbde0d00b9b2d9402
Erick Escagion <erickeescagion@gmail.com>
2021-06-18 14:38:53 -0300

    create a table

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,12 @@
 import React, {useState} from 'react'
 import{Redirect} from 'react-router-dom'
-import Table from './Table';
-
+import Table from './Table/Table';
 const Main = () =>{
+
+    const data = [
+        { id:1, name:'Erick', telephone:'(15)99818-1242', sex:'M'},
+        { id:2, name:'Laura', telephone:'(15)12345-6789', sex:'F'}
+    ]
 
     const [redirectToRegister, setRedirectToRegister] = useState(false);
     const [redirectToChange, setRedirectToChange] = useState(false);
@@ -15,6 +19,10 @@ const Main = () =>{
         setRedirectToChange(true);
     }
 
+    const del = (oEvent) =>{
+        console.log("Deleta");
+    }
+
     const renderRedirect = () =>{
         if(redirectToRegister === true){
             return <Redirect to='/register'/>;
@@ -24,15 +32,22 @@ const Main = () =>{
         return null;
     }
 
+    const head = {
+        id: 'Codigo',
+        name: 'Nome',
+        telephone: 'Telefone',
+        sex: 'Sexo'
+    }
+
     return (
         <>
         {renderRedirect()}
         <div className="main">
             <input type="button" value="Cadastrar" onClick={register}></input>
             <input type="button" value="Alterar" onClick={change}></input>
-            <input type="button" value="Excluir"></input>
+            <input type="button" value="Excluir" onClick={del}></input>
         </div> 
-        <Table></Table>
+        <Table data={data} head={head}/>
         </>
     )
 } 
---
+++ src/components/Table/Table.js
@@ -0,0 +1,41 @@
+import React from 'react'
+
+const Head = ({keys, head}) =>{
+    const tableHead = head || {}
+    return(
+        <thead>
+            <tr>
+            <label/>
+                {
+                    keys.map(key => <th key={key}>{tableHead[key] || key}</th>)
+                }
+            </tr>
+        </thead>
+    )
+}
+const Row = ({record}) =>{
+    const keys = Object.keys(record);
+    return (
+        <tr key={record.id}>
+            <label><input type="checkbox" /></label>
+            {
+                keys.map(key => <td key={key}>{record[key]}</td>)
+            }
+        </tr>
+    )
+}
+const Table = ({ data, head }) =>{
+    const keys = Object.keys(data[0])
+    return (
+        <div className="table">
+            <table>
+                <Head keys={keys} head={head} />
+                <tbody>
+                    { data.map(record => <Row record={record}/>) }
+                </tbody>
+            </table>
+        </div>
+    )
+} 
+
+export default Table;
