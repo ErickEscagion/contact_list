ba73e591c1a03e35dd5fac6edfa7fe2c07eda4a4
Erick Escagion <erickeescagion@gmail.com>
2021-06-18 10:56:17 -0300

    Table component created

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <h1>Lista de Contatos</h1>
         <Switch>
           <Route path="/" exact component={Buttons} />
         </Switch>
