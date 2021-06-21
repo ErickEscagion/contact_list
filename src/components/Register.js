1f1904689146ea20b39135595b94235d73154fd0
Erick Escagion <erickeescagion@gmail.com>
2021-06-21 09:07:20 -0300

    register component created

--- src/components/Register.js
+++ src/components/Register.js
@@ -18,12 +18,29 @@ const Register = () =>{
     return (
         <>
         {renderRedirect()}
-        <div>
-            <h1>Cadastrar</h1>
-        </div>
-        <div className="register">
-            <input type="button" value="Salvar"></input>
-            <input type="button" value="Sair" onClick={exit}></input>
+        <div id="register" className="registerCSS"> 
+            <form method="post" action=""> 
+                <h1>Cadastrar</h1> 
+                <label for="name">Nome:</label>
+                <br/>
+                <input required="required" type="text" placeholder="Nome" />
+                <br/>
+                <label for="telephone">Telefone:</label>    
+                <br/>         
+                <input required="required" type="text" placeholder="(xx)xxxxx-xxxx"/> 
+                <br/>
+                <label for="sex">Sexo:</label>    
+                <br/>
+                <input type="radio" id="male" name="sex" value="male"/>
+                <label for="male">Masculino</label>
+                <input type="radio" id="female" name="sex" value="female"/>
+                <label for="female">Femenino</label>
+                <input type="radio" id="other" name="sex" value="other"/>
+                <label for="other">Outros</label>
+                <br/>
+                <input type="button" value="Salvar"></input>
+                <input type="button" value="Sair" onClick={exit}></input>
+            </form>
         </div>
         </>
     )
